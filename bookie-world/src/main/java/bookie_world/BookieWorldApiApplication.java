19ebe9f70f80cbdac072d122068e6f6af1c311cd
Hui Yang <hui.yang.9045@gmail.com>
2025-07-26 19:08:36 -0400

    created backend spring boot project for my first full stack web app

---
+++ bookie-world/src/main/java/bookie_world/BookieWorldApiApplication.java
@@ -0,0 +1,13 @@
+package bookie_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookieWorldApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookieWorldApiApplication.class, args);
+	}
+
+}
---
+++ bookie-world/src/test/java/bookie_world/BookieWorldApiApplicationTests.java
@@ -0,0 +1,13 @@
+package bookie_world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookieWorldApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
